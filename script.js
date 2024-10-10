694f5f1cea508c9b4a699b263140fa93bc01d8cc
ForgotAlready <oleg.burdakin2016@yandex.ru>
2024-10-11 00:38:08 +0700

    final commit

--- script.js
+++ script.js
@@ -1,19 +1,83 @@
-function createAdditionalInfo(header)
+const veryLongString = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet asperiores aut nihil! Corporis debitis labore fugiat id, eligendi ratione veritatis!"
+const anotherVeryLongString = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam hic, ipsa, ullam, cupiditate eveniet at voluptate corrupti commodi nobis ratione voluptatem! Vel animi totam cupiditate doloribus ad ab exercitationem officia eveniet impedit? Deleniti quasi nisi consectetur perspiciatis quibusdam nostrum, enim perferendis nam, magni molestias recusandae id libero vitae, repudiandae praesentium."
+
+const mapping = 
+{
+    "Новость 1" : veryLongString + "<br/>" + anotherVeryLongString,
+    "Новость 2" : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. A, alias" + "<br/>" + anotherVeryLongString,
+    "Новость 3" : veryLongString + "<br/>" + anotherVeryLongString
+}
+
+function createWrapper()
+{
+    let infoWrapper = document.createElement("div")
+    infoWrapper.className = "pop-up-wrapper"
+    return infoWrapper
+}
+
+function createInfoBlock()
 {
     let info = document.createElement("div")
-    info.appendChild(header)
-    document.querySelector("body").appendChild(info)
+    info.className = "pop-up"
+    return info
+}
 
-    info.style.position = "fixed"
-    info.style.top = 0
-    mainBlock = document.querySelector("main")
-    mainBlock.style.opacity = 0.5;
+function createHeader(headerText)
+{
+    let header = document.createElement("h2")
+    header.style.marginBottom = "20px"
+    header.innerHTML = headerText
+    return header
+}
+
+function createMainBlock(mainBlockText)
+{
+    let infoMainBlock = document.createElement("div")
+    infoMainBlock.innerHTML = mainBlockText
+    return infoMainBlock
+}
+
+function createAdditionalInfo(headerText, mainBlockText)
+{
+
+    let infoWrapper = createWrapper()
+    let info = createInfoBlock()
+
+    let header = createHeader(headerText)
+    let infoMainBlock = createMainBlock(headerText + ": " + mainBlockText)
+
+    info.appendChild(header)
+    info.appendChild(infoMainBlock)
+
+    infoWrapper.appendChild(info)
+    document.querySelector("body").appendChild(infoWrapper)
+
+    let mainBlock = document.querySelector("main")
+    mainBlock.style.opacity = 0.2;
+}
+
+function getHeader(elem)
+{
+    let newsBlock = elem.parentNode.parentNode
+    let header = newsBlock.querySelector(".newsBoxHeader > h2")
+    return header
 }
 
 function showWindow(clicked)
 {
-    let newsBlock = clicked.parentNode.parentNode
-    let header = newsBlock.querySelector(".newsBoxHeader > h2")
-    console.log(header.innerHTML)
-    createAdditionalInfo(header.innerHTML)
+    if(document.querySelector(".pop-up") == null)
+    {
+        let header = getHeader(clicked)
+        createAdditionalInfo(header.textContent, mapping[header.textContent.trim()])
+    }
 }
+
+window.addEventListener('click', function(e)
+{
+    let additinalInfo = document.querySelector('.pop-up-wrapper')
+    if(additinalInfo != null && e.target.tagName != 'BUTTON')
+    {
+        additinalInfo.remove()
+        document.querySelector("main").style.opacity = 1.0
+    }
+})
